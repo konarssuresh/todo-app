d49d9234397b8e396e5b6d688b31751fbbc58db1
Konar S Suresh <konarssuresh@gmail.com>
2017-07-07 12:49:13 +0530

    First commit

---
+++ assets/js/todo.js
@@ -0,0 +1,21 @@
+$("ul").on("click","li",function(){
+$(this).toggleClass("selected");
+});
+
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut("500",function(){
+ 	$(this).remove();
+    });
+   event.stopPropagation();
+});
+$("input[type=text]").keypress(function(event){
+	if(event.which === 13)
+	{
+        var text=$(this).val();
+        $(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> "+text+" </li>");
+	}
+});
+$(".fa-plus").on("click",function(){
+$("input[type=text]").fadeToggle("1");
+});
